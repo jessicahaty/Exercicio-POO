faa3a9665e713084caa7d75ed640f731fdaae202
jessicahaty <jessicaisibasi@gmail.com>
2023-08-26 15:47:07 -0300

    Utilizando getter e setter

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 class Ingrediente {
     constructor(nome, quantidade) {
-        this.nome = nome
-        this.quantidade = quantidade
+        this._nome = nome
+        this._quantidade = quantidade
     }
+
+    get nome() {
+        return this._nome
+    }
+
+    set nome(novoNome) {
+        this._nome = novoNome
+    }
+
+    get quantidade() {
+        return this._quantidade
+    }
+
+    set quantidade(novaQuantidade) {
+        this._quantidade = novaQuantidade
+    }
+
+    estoque() {
+        console.log(`O ingrediente ${this.nome} ainda possui ${this.quantidade} no estoque`)
+    }
+
 }
 
-const Chocolate = new Ingrediente("Chocolate", 3)
+const chocolate = new Ingrediente("Chocolate", 3)
+const farinha = new Ingrediente("Farinha", 10)
 
-console.log(Chocolate)
+chocolate.nome = "Chocolate branco"
+chocolate.quantidade = 1
+
+console.log(chocolate)
+
+chocolate.estoque()
+farinha.estoque()
